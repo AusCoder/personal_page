5c465dac037255050c459be7ef5c494736aff016
Sebastian Mueller <smueller@republicwealth.com.au>
2016-10-25 16:08:24 +1100

    added growing transition to the random walk

--- scripts/client.js
+++ scripts/client.js
@@ -2,10 +2,16 @@
 import 'pw-scripts/debug_config';
 import 'pw-styles';
 
-import 'pw-scripts/brownian_motions/brownian2d';
+import { draw } from 'pw-scripts/brownian_motions/brownian2d';
 
 import $ from 'jquery';
 
 $(document).ready(() => {
   console.log('document is ready!');
+  draw();
+  console.log("drew!");
+  $(window).on('resize', function () {
+    $('#brownian2d').empty();
+    draw();
+  });
 });
