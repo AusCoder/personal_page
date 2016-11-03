5cb41ee8c99373a2654c8c7bcc8131bd4d2c58a1
Sebastian Mueller <smueller@republicwealth.com.au>
2016-11-04 10:57:20 +1100

    added fancy header button borders

--- styles/index.js
+++ styles/index.js
@@ -2,3 +2,4 @@ import '!style!css!font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '!style!css!sass!./client.scss';
+import '!style!css!sass!./fancy_button.scss';
