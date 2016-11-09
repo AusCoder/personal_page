ac5c7afc439b9438a0e31a9b0219d1e2bbad7275
Sebastian Mueller <smueller@republicwealth.com.au>
2016-11-09 16:42:06 +1100

    small styling fix

--- styles/index.js
+++ styles/index.js
@@ -2,4 +2,3 @@ import '!style!css!font-awesome/css/font-awesome.min.css';
 import '!style!css!bootstrap/dist/css/bootstrap.min.css';
 
 import 'pw-styles/client.scss';
-import 'pw-styles/fancy_button.scss';
